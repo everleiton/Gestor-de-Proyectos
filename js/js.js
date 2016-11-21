eb9e12ebeb8d6b0cd3a3dfbe14737b1f2ae912d7
Ever Leiton <everleitonm@gmail.com>
2016-11-21 04:59:55 -0600

    commit

--- js/js.js
+++ js/js.js
@@ -1,25 +1,44 @@
 
 $(document).ready(function() {
-$(function() {
-  $('.btnherramenta > div').hover(function() {
-    $(this).css('margin-left', '0px'); 
-  }, function() {
-    // vuelve a dejar el <div> como estaba al hacer el "mouseout"
-    $(this).css('margin-left', '-100px');
+  eliminarElemento();
+
+  $('.tooltipped').tooltip({delay: 50});
+  
+  $('.modal').modal();
+  
+
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
   });
+  $("#undo").on('click', function(){
+    $(this).unbind();
+  });
+  
+  $( "#agregarproyecto" ).on( "click", function() {
+    $('.tareas').append('<div class="card fdv" ><div class="card-content">'+
+            '<div class="texto"><p class="flow-text">Proyecjnjknkjnto Web I</p></div>'+
+            '<div class="iconos">'+
+            '  <a id="editCard"class="btn-floating tooltipped" data-position="top" data-delay="50" data-tooltip="Editar"><i class="material-icons blue '+ 'lighten+1">edit</i></a>'+
+            '  <a class="btn-floating tooltipped deleteCard" data-position="right" data-delay="50" data-tooltip="Eliminar"><i class="material-icons blue '+ 'lighten+1">delete</i></a>'+
+          '  </div></div><div class="card-action"> Nuevo | En Proceso |Finali</div></div>');
+    $('.tooltipped').tooltip({delay: 50});
+    eliminarElemento();
+  });
+  
+
+  
+  
+  
+  
+  
+  
+  
 });
 
-$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });
-     
-
-
-
-
-/* Al añadir los botones no se añade el tooltipped*/
-$(document).ready(function(){
-   $('.tooltipped').tooltip({delay: 50});
- });
-});
+function eliminarElemento() {
+  $( ".deleteCard" ).on( "click", function() {
+  $(this).parent().parent().parent().slideUp();
+    
+  });  
+}
