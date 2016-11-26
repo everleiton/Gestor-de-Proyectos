de77034023b5c5bd38ab64ee2c39d80bb16db36d
Ever Leiton <everleitonm@gmail.com>
2016-11-25 18:45:35 -0600

    changes

--- js/js.js
+++ js/js.js
@@ -1,32 +1,92 @@
+var proyectonuevo="";
+var listaProyectos = [];
+var Persister = {
+	save: function (key, value) {
+		localStorage.setItem(key, value);
+	},
+	load: function(key, default_value) {
+		return localStorage.getItem(key) || default_value;
+	},
+	saveObj: function(key, value) {
+		var json_string = JSON.stringify(value);
+		this.save(key, json_string);
+	},
+	loadObj: function(key, default_value) {
+		var json_string = this.load(key, default_value);
+		return JSON.parse(json_string);
+	}
+};
+
+function load_data() {
+
+	listaProyectos = Persister.loadObj('listaProyectos', "[]");
+
+	$('#listaProyectos').html('');
+	for (var i = 0; i < listaProyectos.length; i++) {
+		$('.tareas').append(listaProyectos[i].nombre);
+	}
+}
 
 $(document).ready(function() {
-  eliminarElemento();
+  load_data();
 
+  
+  eliminarElemento();
+  editarElemento();
+  
   $('.tooltipped').tooltip({delay: 50});
   
   $('.modal').modal();
   
-
+  
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 5 // Creates a dropdown of 15 years to control year
   });
   $("#undo").on('click', function(){
     $(this).unbind();
   });
   
   $( "#agregarproyecto" ).on( "click", function() {
-    $('.tareas').append('<div class="card fdv" ><div class="card-content">'+
-            '<div class="texto"><p class="flow-text">Proyecjnjknkjnto Web I</p></div>'+
-            '<div class="iconos">'+
-            '  <a id="editCard"class="btn-floating tooltipped" data-position="top" data-delay="50" data-tooltip="Editar"><i class="material-icons blue '+ 'lighten+1">edit</i></a>'+
-            '  <a class="btn-floating tooltipped deleteCard" data-position="right" data-delay="50" data-tooltip="Eliminar"><i class="material-icons blue '+ 'lighten+1">delete</i></a>'+
-          '  </div></div><div class="card-action"> Nuevo | En Proceso |Finali</div></div>');
-    $('.tooltipped').tooltip({delay: 50});
-    eliminarElemento();
+    event.preventDefault();
+    
+      var nomNuevoPro= document.getElementById("nameProyecto").value;
+        var fechaInicio= document.getElementById("dateInicio").value;
+        var ID= Math.random().toString(36).substr(2, 4);
+        proyectonuevo='	  <div class="card" >	'+
+        '<div class="card-content objMovible">	'+
+        '<div class="texto"><p id="proyecto_234"class="'+ID+'">'+nomNuevoPro+'</p></div>	'+
+        '<div class="iconos">	'+
+        '<a id="proyecto_234" href="#modal_editar"class="btn-floating tooltipped proyecto_234" data-position="top" data-delay="50" data-tooltip="Editar"><i class="material-icons blue lighten+1">edit</i></a>	'+
+        '<a class="btn-floating tooltipped deleteCard" data-position="right" data-delay="50" data-tooltip="Eliminar"><i class="material-icons blue lighten+1">delete</i></a>	'+
+        '	 </div>	'+
+        '</div>	'+
+        '<div class="card-action">	'+
+        '<h6>ID: <strong>'+ID+'</strong><span class="new badge blue lighten+1" data-badge-caption="Personas a cargo">0</span></h6>	'+
+        ' <h6>Fecha de Inicio: '+fechaInicio+'</h6>	'+
+        '</div>	'+
+        '<div class="card-action"> Nuevo | En Proceso |Finali</div>	'+
+        '</div>	';
+        if ((nomNuevoPro != "") && fechaInicio !="") {
+          listaProyectos.push({nombre: proyectonuevo});
+          Persister.saveObj('listaProyectos', listaProyectos);
+        
+          $('.tareas').append(proyectonuevo);
+          $('.tooltipped').tooltip({delay: 50});
+          editarElemento();
+          eliminarElemento();
+      } else {
+          $("#nameProyecto").attr("placeholder", "Campo Vacío");
+      }
+    
+    
+  
+    
   });
   
-
+  
+  
+  
   
   
   
@@ -36,9 +96,22 @@ $(document).ready(function() {
   
 });
 
+
 function eliminarElemento() {
+  
   $( ".deleteCard" ).on( "click", function() {
-  $(this).parent().parent().parent().slideUp();
+    $(this).parent().parent().parent().slideUp();
+    
+  });  
+}
+
+
+function editarElemento() {
+  
+  $( "#salvarEdicion" ).on( "click", function() {
+    var nombre= document.getElementById("nombreEdicion").value;
+    document.getElementById("proyecto_234").innerHTML = nombre;
+    $('#nombreEdicion').val("");
     
   });  
 }
---
+++ js/mover.js
@@ -0,0 +1,50 @@
+var estoyArrastrando = false;
+//Variable para almacenar un puntero al objeto que estamos moviendo
+var dobj;
+function presionarBoton(e) {
+  //Obtenemos el elemento sobre el que se ha presionado el botón del ratón
+  var fobj = moz ? e.target : event.srcElement;
+
+  // Buscamos el primer elemento en la que esté contenido aquel sobre el que se ha pulsado
+  // que pertenezca a la clase objMovible. Esto es necesario por si hemos pinchando sobre
+  // un elemento contenido dentro de otro pero este último es el que pertenece a la clase
+  // objmovible
+  while (fobj.tagName.toLowerCase() != "html" && fobj.className != "objMovible") {
+    fobj = moz ? fobj.parentNode : fobj.parentElement;
+  }
+
+  // Si hemos obtenido un objeto movible...			
+  if (fobj.className == "objMovible") {
+    // Activamos el flag para indicar que se empieza a arrastrar
+    estoyArrastrando = true;
+    // Guardamos un puntero al objeto que se está moviendo en la variable global
+    dobj = fobj;
+    // Devolvemos false para no realizar ninguna acción posterior
+    return false;
+  }
+}
+//Asociamos la función al evento onmousedown
+document.onmousedown = presionarBoton;
+function arrastrarRaton(e){
+  if (estoyArrastrando) {
+    // Obtenemos las coordenadas X e Y del ratón (de forma diferente dependiendo del navegador del cliente)
+    newLeft = moz ? e.clientX : event.clientX;
+    newTop = moz ? e.clientY : event.clientY;
+
+    // Posicionamos el objeto en las nuevas coordenadas y aplicamos unas desviaciones
+    // horizontal y vertical correspondientes a la mitad del ancho y alto del elemento
+    // que movemos para colocar el puntero en el centro de la capa movible.
+    dobj.style.left = newLeft - parseInt(dobj.style.width)/2;
+    dobj.style.top = newTop - parseInt(dobj.style.height)/2;
+
+    // Devolvemos false para no realizar ninguna acción posterior
+    return false;
+  }
+}
+//Asociamos la función al evento onmousemove
+document.onmousemove = arrastrarRaton;
+function soltarBoton(e) {	
+  estoyArrastrando = false;
+}
+//Asociamos la función al evento onmouseup
+document.onmouseup = soltarBoton;
